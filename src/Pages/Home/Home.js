06b682acaed63e637a97c56b861b3f1f194e0a78
ahmed-nasir <nasirahmed595@gmail.com>
2022-05-13 12:26:18 +0600

    Add info card

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Banner from './Banner';
+import Infoo from './Infoo';
+
 
 const Home = () => {
     return (
         <div>
             <h2>This is home</h2>
             <Banner></Banner>
+            <Infoo></Infoo>
         </div>
     );
 };
