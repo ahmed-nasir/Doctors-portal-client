e353ce08429b3651148757050a2f888c037204ea
ahmed-nasir <nasirahmed595@gmail.com>
2022-05-15 07:47:20 +0600

    set modal & bring data on modal

--- src/Pages/Home/Exception.js
+++ src/Pages/Home/Exception.js
@@ -8,7 +8,7 @@ const Exception = () => {
             <div className="hero-content flex-col lg:flex-row">
                 <img height='400px' width='400px' className='rounded-md' src={treatment} />
                 <div className='ml-5'>
-                    <h1 className="text-5xl font-bold">Exceptional Dental Care, on Your Terms</h1>
+                    <h1 className="text-xl lg:text-5xl font-bold">Exceptional Dental Care, on Your Terms</h1>
                     <p className="py-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
                     <PrimaryButton>Get Started</PrimaryButton>
                 </div>
