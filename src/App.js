6cebdf86918d35fdd3e41ae646ada20ce0e449e9
ahmed-nasir <nasirahmed595@gmail.com>
2022-05-16 09:54:12 +0600

    Implement Requirauth & redirect user to the page they wanted to go

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Pages/Shared/Navbar';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes} from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
 import Appointment from './Pages/Appointment/Appointment';
@@ -9,19 +9,24 @@ import Reviews from './Pages/Reviews/Reviews';
 
 import Login from './Pages/Login/Login';
 import SignUp from './Pages/Login/SignUp';
+import RequirAuth from './Pages/Login/RequirAuth';
 
 function App() {
   return (
     <div className="max-w-7xl mx-auto px-12">
       <Navbar></Navbar>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/about' element={<About/> }/>
-        <Route path='/appointment' element={<Appointment/> }/>
-        <Route path='/reviews' element={<Reviews/> }/>
-        
-        <Route path='/login' element={<Login/> }/>
-        <Route path='/signup' element={<SignUp/> }/>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/appointment' element={
+          <RequirAuth>
+            <Appointment />
+          </RequirAuth>
+        } />
+        <Route path='/reviews' element={<Reviews />} />
+
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
       </Routes>
 
     </div>
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -3,7 +3,7 @@ import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-fireba
 import auth from '../../firebase.init';
 import { useForm } from "react-hook-form";
 import Loading from '../Shared/Loading';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 const Login = () => {
     const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
@@ -17,6 +17,10 @@ const Login = () => {
 
     let signInError;
 
+    const navigate = useNavigate();
+    const location = useLocation();
+    let from = location.state?.from?.pathname || "/";
+
     if (loading || gLoading) {
         return <Loading></Loading>
     }
@@ -26,12 +30,13 @@ const Login = () => {
     }
 
     if (user || gUser) {
-        console.log(user || gUser);
+        navigate(from, { replace: true });
     }
 
     const onSubmit = data => {
         console.log(data);
         signInWithEmailAndPassword(data.email, data.password)
+
     }
     return (
         <div className='flex justify-center items-center h-screen'>
